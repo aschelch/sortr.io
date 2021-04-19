7c13bac4994173239846a457df31b518b2cfec58
Aurélien Schelcher <aurelien.schelcher@gmail.com>
2021-04-19 22:37:22 +0200

    feat(sort): add class when using keyboard

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,14 @@
 document.onkeydown = function(e) {
     switch (e.keyCode) {
         case 37:
-            document.getElementById('left-win').click();
+            el = document.getElementById('left-win');
+            el.classList.add('is-active');
+            el.click();
             break;
         case 39:
-            document.getElementById('right-win').click();
+            el = document.getElementById('right-win');
+            el.classList.add('is-active');
+            el.click();
             break;
     }
 };
